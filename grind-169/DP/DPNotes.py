fabff9ee9ee848076f2ebb51d928094b54a6e5fc
domfernando319 <domfernando319@gmail.com>
2024-09-19 22:39:19 -0400

    Day 3 ASI

--- grind-169/DP/DPNotes.py
+++ grind-169/DP/DPNotes.py
@@ -40,3 +40,54 @@ def fib(n, memo):
   memo[n] = result
   return result
 
+# By using memo structure, we cut down on recrusive calls and going down recursive subtrees
+
+
+def gridTraveler(n, m):
+  if n == 1 and m == 1:
+    return 1
+  if n == 0 or m == 0:
+    return 0 
+  
+  moveDown = gridTraveler(n-1, m)
+  moveRight = gridTraveler(n, m-1)
+  return moveDown + moveRight
+  
+# again this will be slow for a large grid 
+# use memo
+memo = {}
+def gridTraveler(n, m, memo):
+  key = n + ',' + m
+  if key in memo:
+    return memo[key]
+  if n == 1 and m == 1:
+    return 1
+  if n == 0 or m == 0:
+    return 0 
+  
+  moveDown = gridTraveler(n-1, m, memo)
+  moveRight = gridTraveler(n, m-1, memo)
+  memo[key] = moveDown + moveRight
+  return moveDown + moveRight
+
+# With memoization we cut down O(2^n+m) to O(n*m) time
+# tree nodes could have m*n combinations
+# O(n+m) Space
+
+# Memoization Suggestion -> DP in general
+"""
+  1. Make it work
+    * visualize the problem as a tree
+    * implement the tree using recursion (leaves of tree are base case for hint)
+    * test it
+
+  2. Make it efficient
+    * add a memo object
+    * add a base case to return memo values
+    * store return values into the memo
+
+  
+"""
+
+
+ 
